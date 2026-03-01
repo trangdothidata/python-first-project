dda4c740b444a581ea54efdd5f04b2d117a21231
trangdothidata <trangdothi739@gmail.com>
2026-03-01 13:13:24 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
 print("I am building my future with Python")
+name = "Trang"
+age = 17
+goal = "AI Engineer"
+print("My name is", name)
+print("I am", age,"years old")
+print("I want to become an", goal)
