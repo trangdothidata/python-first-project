8e82f684a30ce05d04f3b1f7d3ab8d382eceebff
trangdothidata <trangdothi739@gmail.com>
2026-03-01 13:18:30 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,3 +5,13 @@ goal = "AI Engineer"
 print("My name is", name)
 print("I am", age,"years old")
 print("I want to become an", goal)
+a = 10
+b = 5
+print("a+b", a+b)
+print("a-n", a-b)
+print("a/b", a/b)
+score = 8 
+if score >=8:
+  print("You are excellent")
+else: 
+  print("Keep improving!")
